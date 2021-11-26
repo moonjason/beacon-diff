e2e170832df44dec9856a299d24edbea18e394b8
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2021-11-26 12:21:24 +0000

    :sparkles: add tabs

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
 import './App.css';
-import CodeDiff from './CodeDiff';
+import SimpleTabs from './Tabs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <div style={{
-        height: "100vh",
-        width: "100vw",
-        backgroundColor: "red"
-      }}>
-        <CodeDiff
-          language="json"
-          originalValue={JSON.stringify({})}
-          modifiedValue={JSON.stringify({})}
-          options={{
-            theme: "vs",
-            height: "100%",
-            width: "100%",
-            scrollbar: {
-              vertical: "auto",
-              horizontal: "auto"
-            },
-            automaticLayout: true,
-            minimap: {
-              enabled: false
-            },
-            enableSplitViewResizing: false,
-            readOnly: true
-          }} />
-        </div>
+      <SimpleTabs />
     </div>
   );
 }
