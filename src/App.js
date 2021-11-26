4f034b397e7d211d98b1285d4f0102f8f488ef3d
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2021-11-26 12:39:32 +0000

    :hammer: convert App to class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { Component } from 'react';
 import './App.css';
 import SimpleTabs from './Tabs';
 
-function App() {
-  return (
-    <div className="App">
-      <SimpleTabs />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      control: {
+
+      },
+      variant: {
+
+      }
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <SimpleTabs />
+      </div>
+    );
+  }
 }
 
 export default App;
